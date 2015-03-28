c79fd8b25a487fc7fcf57a57136ba44ee6295356
lomagno2003 <lomagno2003@gmail.com>
2015-03-28 15:34:12 -0300

    Push, Put, Get all y Delete en News andando
    Incluidas transferencias de archivos

--- src/controllers/news/NewsControllerProvider.php
+++ src/controllers/news/NewsControllerProvider.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManager;
 use tvtandil\model\entities\News;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use tvtandil\model\entities\tvtandil\model\entities;
 
 class NewsControllerProvider implements ControllerProviderInterface {
 	public function connect(Application $app) {
@@ -28,20 +29,50 @@ class NewsControllerProvider implements ControllerProviderInterface {
 			return $app ['serializer']->serialize ( $result, 'json' );
 		} );
 		
+		$controllers->post ( '/', function (Application $app, Request $request) {
+			$data = json_decode ( $request->getContent (), true );
+			
+			$explodedData = explode(',', $data['file']);
+			$fileData = base64_decode($explodedData[1]);
+			file_put_contents('./test.json', $fileData);
+			
+			$news = new News ();
+			
+			$news->setTitle ( $data ['title'] );
+			$app ['orm.em']->persist ( $news );
+			$app ['orm.em']->flush ();
+			return new Response ( 200 );
+		} );
+		
 		$controllers->put ( '/{id}', function (Application $app, Request $request, $id) {
-			$data = json_decode($request->getContent(), true);
+			$data = json_decode ( $request->getContent (), true );
 			
 			$newsRepository = $app ['orm.em']->getRepository ( 'tvtandil\model\entities\News' );
 			$news = $newsRepository->find ( $id );
 			
-			$news->setTitle ( $data['title']);
-			$app ['orm.em']->flush();
+			$news->setTitle ( $data ['title'] );
+			$app ['orm.em']->flush ();
 			return new Response ( 200 );
 		} );
 		
-		$controllers->match('/{id}', function(){
-			return new Response ( 205 );
-		})->method('OPTIONS');
+		$controllers->delete ( '/{id}', function (Application $app, Request $request, $id) {
+			$data = json_decode ( $request->getContent (), true );
+			
+			$newsRepository = $app ['orm.em']->getRepository ( 'tvtandil\model\entities\News' );
+			$news = $newsRepository->find ( $id );
+			
+			$app ['orm.em']->remove($news);
+			$app ['orm.em']->flush ();
+			return new Response ( 200 );
+		} );
+		
+		/* Allow OPTION method for all the used paths */
+		$controllers->match ( '/{id}', function () {
+			return new Response ( 200 );
+		} )->method ( 'OPTIONS' );
+		$controllers->match ( '/', function () {
+			return new Response ( 200 );
+		} )->method ( 'OPTIONS' );
 		
 		return $controllers;
 	}
